b7666ef797d6e8d7098fe72534b5b1230d54ddec
Clement <Clementnjk@gmail.com>
2021-10-29 17:08:42 +0800

    Typeracer skeleton code 🦴

---
+++ modules/games/typeracer.py
@@ -0,0 +1,17 @@
+from typing import Type
+from dis_snek.models import Scale
+from dis_snek.models.application_commands import (
+    OptionTypes,
+    slash_command,
+    slash_option,
+)
+from dis_snek.models.context import InteractionContext
+from dis_snek.models.discord_objects.embed import EmbedAttachment
+from dis_snek.models.discord_objects.embed import Embed
+
+class Typeracer(Scale):
+    def __init__(self, bot):
+        self.bot = bot
+
+def setup(Bot):
+    Typeracer(Bot)
