8e3cf4d46788159605d057b3d39314edb86670bb
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-10-30 15:20:54 +0800

    Type hint and convert env vars.

--- utils/config.py
+++ utils/config.py
@@ -1,14 +1,15 @@
 import logging
 from os import environ
 
+from dis_snek.models import to_snowflake
 from dotenv.main import load_dotenv
 
 
 load_dotenv()
 
 
-TOKEN = environ.get("TOKEN")
-GUILD = environ.get("GUILD")
-ADMIN_ROLE = environ.get("ADMIN_ROLE")
-BOT_DEV_ROLE = environ.get("BOT_DEV_ROLE")
-LOGGING_LEVEL = environ.get("LOGGING_LEVEL", logging.DEBUG)
+TOKEN: str = environ.get("TOKEN")
+GUILD: int = to_snowflake(environ.get("GUILD"))
+ADMIN_ROLE = to_snowflake(environ.get("ADMIN_ROLE"))
+BOT_DEV_ROLE: int = to_snowflake(environ.get("BOT_DEV_ROLE"))
+LOGGING_LEVEL: int = environ.get("LOGGING_LEVEL", logging.DEBUG)
