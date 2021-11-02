2cc058feffc06ce0459ece680f9a718369314cb4
Clement <Clementnjk@gmail.com>
2021-11-02 14:05:06 +0800

    Ticket implementation

--- modules/genius/genius.py
+++ modules/genius/genius.py
@@ -8,15 +8,21 @@ from dis_snek.models.application_commands import (
 from dis_snek.models.context import InteractionContext
 from dis_snek.models.discord_objects.embed import EmbedAttachment, EmbedField
 from dis_snek.models.discord_objects.components import Button, ActionRow
-from dis_snek.models.discord_objects.channel import GuildText
+from dis_snek.models.discord_objects.channel import GuildText, PermissionOverwrite
+from dis_snek.models.discord_objects.guild import Guild
 from dis_snek.models.enums import ButtonStyles
 from dis_snek.models.discord_objects.embed import Embed
 from dis_snek.models.listener import listen
+from dis_snek.http_requests.channels import ChannelRequests
 
+from utils.config import  GUILD
 
 class Genius(Scale):
     def __init__(self, bot):
         self.bot = bot
+        self.occupied = {}
+        self.queue = []
+        self.maxTickets = 2
 
     @slash_command("genius_setup", "Setup the Genius Bar in a text channel")
     @slash_option(
@@ -55,12 +61,12 @@ class Genius(Scale):
         button1 = Button(
             style=ButtonStyles.BLURPLE, 
             label="Queue up", emoji="▶",
-            custom_id="getInQueue") #Camel case good, dont @ me
+            custom_id="getIn") #Camel case good, dont @ me
 
         button2 = Button(
             style=ButtonStyles.RED,
             label="De-queue",
-            custom_id="getOutQueue"
+            custom_id="getOut"
         )
 
         await channel.send(
@@ -69,7 +75,81 @@ class Genius(Scale):
 
         await ctx.send("Queue setup complete")
 
+    @component_callback("getIn")
+    async def get_in(self, ctx):
+        user: int = ctx.author.id
 
+        if user in self.occupied or user in self.queue:
+            await ctx.send("Only 1 ticket at a time please!!", ephemeral=True)
+            return
+
+        if len(self.occupied) > self.maxTickets:
+            self.queue.append(user)
+            await ctx.send("We're at our limit at the moment, we've queued you up and will give you a ping when we're ready!", ephemeral=True)
+            return
+
+        await self.create_ticket(user)
+
+        await ctx.send("Ticket created!", ephemeral=True)
+
+
+    @component_callback("closeTicket")
+    async def close_ticket(self, ctx: InteractionContext):
+        channels = self.occupied[ctx.author.id]
+        if channels[0] != ctx.channel.id: # User did not create ticket
+            return
+
+        g: Guild = await self.bot.get_guild(GUILD)
+            
+        await g.delete_channel(channels[0])
+        await g.delete_channel(channels[1])
+        await g.delete_channel(channels[2])
+
+        if len(self.queue) == 0: #no-one in queue
+            return
+
+        await self.create_ticket(self, self.queue[0])
+
+
+            
+
+
+
+    async def create_ticket(self, user):
+        g: Guild = await self.bot.get_guild(GUILD)
+        cat = await g.create_category(
+            f"ticket-{user}",
+            position=999,
+        )
+        ChannelRequests.edit_channel_permission(
+            cat.id,
+            user,
+            allow="1200",
+            deny="0",
+            perm_type=1
+        )
+        tc = await g.create_text_channel(
+            f"ticket-{user}",
+            category=cat.id
+        )
+        vc = await g.create_voice_channel(
+            f"ticket-{user}",
+            category=cat.id
+        )
+
+        self.occupied[user] = (tc.id, vc.id, cat.id)
+
+        button3 = Button(
+            style=ButtonStyles.RED,
+            label="Close ticket",
+            custom_id="closeTicket"
+        )
+
+        await tc.send(
+            f"Hey <@{user}>! Welcome to your support channel! Please explain your issue here and someone will help you shortly. Alternatively, join your assigned vc.", # This text was definitely not stolen ;)
+            components=[button3]
+            ) 
+        
 
 
 def setup(Bot):
