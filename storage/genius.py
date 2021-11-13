46e23cde677012521304c69c1163b1e8d89a23a4
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-11-13 17:54:07 +0800

    Add genius bar storage methods.

--- storage/genius.py
+++ storage/genius.py
@@ -2,19 +2,29 @@ from typing import List, Any, Dict, Union
 
 import attr
 from dis_snek.mixins.serialization import DictSerializationMixin
-from dis_snek.models import to_snowflake_list, User, Member
+from dis_snek.models import to_snowflake_list, User, Member, to_snowflake
 
 
 @attr.s(slots=True)
 class Genius(DictSerializationMixin):
     queue: List[int] = attr.ib(factory=list, converter=to_snowflake_list)
-    active: List[int] = attr.ib(factory=list, converter=to_snowflake_list)
+    occupied: Dict[str, int] = attr.ib(factory=dict)
 
     async def load_discord_objects(self, bot):
         pass
+
+    @property
+    def queue_msg(self):
+        return
 
     def enqueue(self, user: Union[User, Member]):
         self.queue.append(user.id)
 
     def dequeue(self) -> int:
         return self.queue.pop(0)
+
+    def new_ticket(self, user, category):
+        self.occupied[str(to_snowflake(user))] = to_snowflake(category)
+
+    def close_ticket(self, user):
+        return self.occupied.pop(str(to_snowflake(user)))
