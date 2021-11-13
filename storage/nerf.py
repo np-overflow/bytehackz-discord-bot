0280addb2e5d7f8892b22aa97bddb70dc3f35fc9
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2021-11-13 20:13:06 +0800

    Absract message storage to MessageStore class.

--- storage/nerf.py
+++ storage/nerf.py
@@ -7,6 +7,7 @@ from dis_snek.mixins.serialization import DictSerializationMixin
 from dis_snek.models import to_snowflake_list, Member, User, to_snowflake, Message, ClientObject
 from dis_snek.utils.serializer import no_export_meta
 
+from storage.object_store import MessageStore
 from utils.config import GUILD
 
 
@@ -14,44 +15,29 @@ from utils.config import GUILD
 class Nerf(DictSerializationMixin):
     queue: List[int] = attr.ib(factory=list, converter=to_snowflake_list)
     score: Dict[str, int] = attr.ib(factory=dict)
-    queue_msg_channel: Optional[int] = attr.ib(default=None, converter=optional(to_snowflake))
-    queue_msg_id: Optional[int] = attr.ib(default=None, converter=optional(to_snowflake))
-    leaderboard_msg_channel: Optional[int] = attr.ib(default=None, converter=optional(to_snowflake))
-    leaderboard_msg_id: Optional[int] = attr.ib(default=None, converter=optional(to_snowflake))
-
-    _queue_msg: Message = attr.ib(default=None, metadata=no_export_meta)
-    _leaderboard_msg: Message = attr.ib(default=None, metadata=no_export_meta)
+    queue_message: MessageStore = attr.ib(factory=dict, converter=MessageStore.from_dict)
+    leaderboard_message: MessageStore = attr.ib(factory=dict, converter=MessageStore.from_dict)
 
     async def load_discord_objects(self, bot):
-        guild = await bot.get_guild(GUILD)
-        if self.queue_msg_channel and self.queue_msg_id:
-            channel = await guild.get_channel(self.queue_msg_channel)
-            self._queue_msg = await channel.get_message(self.queue_msg_id)
-
-        if self.leaderboard_msg_channel and self.leaderboard_msg_id:
-            channel = await guild.get_channel(self.leaderboard_msg_channel)
-            self._leaderboard_msg = await channel.get_message(self.leaderboard_msg_id)
+        await self.queue_message.load(bot)
+        await self.leaderboard_message.load(bot)
 
     @property
     def queue_msg(self):
-        return self._queue_msg
+        return self.queue_message.message
 
     @property
     def leaderboard_msg(self):
-        return self._leaderboard_msg
+        return self.leaderboard_message.message
 
     def set_queue_message(self, message: Message):
-        self.queue_msg_channel = message.channel.id
-        self.queue_msg_id = message.id
-        self._queue_msg = message
+        self.queue_message.set(message)
 
     def set_leaderboard_message(self, message: Message):
-        self.leaderboard_msg_channel = message.channel.id
-        self.leaderboard_msg_id = message.id
-        self._leaderboard_msg = message
+        self.leaderboard_message.set(message)
 
     def is_setup_done(self):
-        return self._leaderboard_msg and self._queue_msg
+        return self.leaderboard_message and self.queue_message
 
     def enqueue(self, user: Union[User, Member]):
         self.queue.append(to_snowflake(user))
---
+++ storage/object_store.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+import attr
+from attr.converters import optional
+from dis_snek.mixins.serialization import DictSerializationMixin
+from dis_snek.models import to_snowflake, Message
+
+
+@attr.s(slots=True)
+class MessageStore(DictSerializationMixin):
+    guild_id: Optional[int] = attr.ib(default=None, converter=optional(to_snowflake))
+    channel_id: Optional[int] = attr.ib(default=None, converter=optional(to_snowflake))
+    message_id: Optional[int] = attr.ib(default=None, converter=optional(to_snowflake))
+
+    _message: Optional[Message] = attr.ib(default=None)
+
+    def __bool__(self):
+        return self._message is not None
+
+    @property
+    def message(self):
+        return self._message
+
+    async def load(self, bot):
+        if self.guild_id and self.channel_id and self.message_id:
+            guild = await bot.get_guild(self.guild_id)
+            channel = await guild.get_channel(self.channel_id)
+            self._message = await channel.get_message(self.message_id)
+
+    def set(self, message: Message):
+        self.guild_id = message.guild.id
+        self.channel_id = message.channel.id
+        self.message_id = message.id
+        self._message = message
+
+    def delete(self):
+        self.guild_id = None
+        self.channel_id = None
+        self.message_id = None
+        self._message = None
